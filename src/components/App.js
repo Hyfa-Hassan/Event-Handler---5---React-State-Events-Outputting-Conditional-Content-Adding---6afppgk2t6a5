c252f56ea8df1582d24dbba4f7d6bbb11e7cc76a
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-18 15:16:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  
+  const formC = (e) =>{
+    e.preventDefault();
+  }
+  const disp = () => {
+    console.log("form submitted");
+  }
 
   return (
     <div id="main">
-      <form>
+      <form onChange={(e)=>formC(e)}>
         <label htmlFor='name'>Name</label>
         <input id="name" type={"text"} />
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={disp}>Submit</button>
       </form>
     </div>
   )
